5cfc4b834fc6fa892a465400a983414991e108da
Gabo <gabrielvegachavez12@gmail.com>
2024-04-14 11:49:10 -0600

    Primer Commit

---
+++ Tarea.py
@@ -0,0 +1,20 @@
+print("Ingrese los valores solicitados:")
+N1 = input("Ingrese el numero de vueltas pata el primer arrollado de cables: ")
+N2 = input("Ingrese el numero de vueltas pata el segundo arrollado de cables: ")
+I = input("Tiene la corriente I1 o I2?: ")
+
+if(I = "I1"):
+    I1 = I
+else:
+    I2 = I
+fp = input("Ingrese el factor de apilado: ")
+Sl = input("Ingrese la seccion transversal del entre hierro: ")
+Sc = input("Ingrese la seccion transversal del resto: ")
+A = input("Ingrese el ancho del material ferromagnetico: ")
+L1 = input("Ingrese la longitud de la parte izquiera del circuito: ")
+L2 = input("Ingrese la longitud de la parte derecha del circuito: ")
+L3 = input("Ingrese el alto de entrehierro: ")
+LE = input("Ingrese el ancho de entrehierro: ")
+HB = input("Desea ingresar la ecuación de H'B en forma de curva(1) o de ecuación(2) ")
+
+Mañana consigo una version funcional
